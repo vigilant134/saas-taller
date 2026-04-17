080d866c37be7681efa2d5cd4012c6b00de4a509
vigilant134 <ricowil911@gmail.com>
2026-04-17 09:27:34 -0700

    Actualizar db.js

--- db.js
+++ db.js
@@ -2,10 +2,14 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT), // 🔥 IMPORTANTE
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 
   ssl: {
     rejectUnauthorized: false
