dad01276104f146b77b6f3390bad75e670546624
vigilant134 <ricowil911@gmail.com>
2026-04-17 10:52:27 -0700

    Actualizar db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   port: Number(process.env.DB_PORT), // 🔥 IMPORTANTE
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: 'sass_taller',
 
   waitForConnections: true,
   connectionLimit: 10,
